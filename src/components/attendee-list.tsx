6273a490c3e1dd21f0e5ff751170939758ec41fc
Abílio Soares Coelho <abiliocoelho@gmail.com>
2024-04-07 16:15:00 -0300

    :fire: Feat: Final

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -9,37 +9,93 @@ import {
   MoreHorizontal,
   Search,
 } from 'lucide-react'
-import { ChangeEvent, useState } from 'react'
-import { attendees } from '../data/seed'
+import { ChangeEvent, useEffect, useState } from 'react'
+
+import { api } from '../services/api'
 import { IconButton } from './icon-button'
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
-export function AttendeeList() {
-  const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+type Attendee = {
+  id: string
+  name: string
+  email: string
+  createdAt: Date
+  checkedInAt: Date
+}
 
-  const totalPages = Math.ceil(attendees.length / 10)
+export function AttendeeList() {
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('query')) {
+      return url.searchParams.get('query') ?? ''
+    }
+
+    return ''
+  })
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
+  const [attendees, setAttendees] = useState<Attendee[]>([])
+  const [total, setTotal] = useState(0)
+
+  const totalPages = Math.ceil(total / 10)
+
+  async function getAttendees() {
+    const url = new URL(
+      'http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees',
+    )
+    url.searchParams.set('pageIndex', String(page - 1))
+    search && url.searchParams.set('query', search)
+    const response = await api.get(url.toString())
+    setAttendees(response.data.attendees)
+    setTotal(response.data.total)
+  }
+
+  useEffect(() => {
+    getAttendees()
+  }, [page, search])
+
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString())
+    url.searchParams.set('query', search)
+    window.history.pushState({}, '', url)
+    setSearch(search)
+  }
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString())
+    url.searchParams.set('page', String(page))
+    window.history.pushState({}, '', url)
+    setPage(page)
+  }
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value)
+    setCurrentSearch(event.target.value)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   return (
@@ -51,7 +107,8 @@ export function AttendeeList() {
           <input
             onChange={onSearchInputChanged}
             type="text"
-            className="bg-transparent text-sm flex flex-1 border-none outline-none"
+            value={search}
+            className="bg-transparent text-sm flex flex-1 border-none outline-none focus:ring-0"
             placeholder="Buscar participante"
           />
         </div>
@@ -88,7 +145,7 @@ export function AttendeeList() {
             </tr>
           </thead>
           <tbody>
-            {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
+            {attendees.map((attendee) => {
               return (
                 <tr
                   className=" border-b border-white/10 hover:bg-white/5"
@@ -129,7 +186,7 @@ export function AttendeeList() {
           <tfoot>
             <tr>
               <td colSpan={3} className="py-3 px-4 text-sm text-zinc-300">
-                Mostrando 10 de {attendees.length} itens
+                Mostrando {attendees.length} de {total} itens
               </td>
               <td
                 colSpan={3}
